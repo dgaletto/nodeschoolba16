cadb8b9182570d4156b20692b2c5c63aed3af503
Damian Galetto <damian@prixmates.com>
2016-05-21 16:28:53 -0300

    FILTERED LS. un poco mejor

--- ejercicios/learnyounode/program.js
+++ ejercicios/learnyounode/program.js
@@ -4,7 +4,11 @@ var path = require('path')
 var dir = process.argv[2]
 var ext = process.argv[3]
 
+//Filtra e imprime.
+//Me dio fiaca hacer un metodo mas para imprimir
 var filterByExt = (err, list) => {
+
+	if(err) return console.error(err)
 
 	filteredList = list.filter(
 		(file) => {
@@ -16,6 +20,8 @@ var filterByExt = (err, list) => {
 			return false
 		}
 		)
+
+	return filterByExt
 	
 }
 
